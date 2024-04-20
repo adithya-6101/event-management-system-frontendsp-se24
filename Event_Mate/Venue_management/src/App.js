1d37aeec9501cfbdf6ef6686359edcff9e47947b
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-04-20 04:16:21 -0400

    Update App.js

--- Event_Mate/Venue_management/src/App.js
+++ Event_Mate/Venue_management/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 import HomeBody from "./components/HomeBody.js";
+import InviteFriend from "./components/inviteFriend.js";
+
 //import "./index.css";
 import NavBar from "./components/navBar";
 import Home from './components/Home.js'
@@ -14,8 +16,9 @@ import PlayersPage from "./components/PlayersPage.js";
 import { SearchBar } from "./components/SearchBar.js";
 import VenueDetails from "./components/venueDetails.js";
 import VenuesTable from "./components/ViewPage.js";
-import ReviewBooking from "./components/reviewBooking.js";
-import Paymethod from "./components/Payment.js";
+import FriendInfo from "./components/friend_info.js";
+import UserPage from "./components/userPage.js";
+import EditProfileDetails from "./components/editProfileDetails.js";
 
 
 
@@ -31,13 +34,15 @@ function App() {
           <Route path='/Login' element={<Login />}/>
           <Route path='/Register' element={<Register />} />
           <Route path='/player-page' element={<PlayersPage />} />
+          <Route path='/user-page' element={<UserPage />} />
           <Route path='/reset-password' element={<ResetPassword />}/>
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path='/search' element={<SearchBar />} />
           <Route path='/venues' element={<VenuesTable />}/>
-          <Route path='/review-booking?' element={<ReviewBooking />}/>
-          <Route path='/payment' element={<Paymethod />}/>
           <Route path='/venue/venue-details?' element={<VenueDetails />}/>
+          <Route path='/edit-profile-details' element={<EditProfileDetails />} />
+          <Route path='/invite-friend' element={<InviteFriend />} />
+          <Route path='/friend_info' element={<FriendInfo/>} />
       </Routes>
     </BrowserRouter>
 
